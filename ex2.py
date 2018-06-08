5e8d69d5d6cdcf5f6b3531cb016bda9afe4995dc
alina <zhongluhua@talicai.com>
2018-06-08 20:32:28 +0800

    add a new file ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#a comment,this is so you can read your program later.
+#anything after the # is ignored by python
+
+print "i could have code like this." #and the comment after is ignored
+
+#you can also use a comment to "disable" or comment out a piece of code:
+#print "this wont't run."
+
+print "this will run."
