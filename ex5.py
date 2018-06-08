d0ff01b4e549110eb46788873ab296157bdd765a
alina <alina@163.com>
2018-06-09 00:04:40 +0800

    add ex5.py

---
+++ ex5.py
@@ -0,0 +1,36 @@
+# --coding:utf-8--
+
+# my_name = 'Zed A. Shaw'
+# my_age = 35 # not a lie
+# my_height = 74 # inches
+# my_weight = 180 # lbs
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Brown'
+
+# print "let's talk about %s." % my_name
+# print "he's %d inches tall." % my_height
+# print "he's %d pounds heavy." % my_weight
+# print "actully that's not too heavy"
+# print "he's got %s eyes and %s hair." % (my_eyes,my_hair)
+# print "his teeth are usually %s depending on the coffee." % my_teeth
+
+#this line is tricky,try to get it exactly right
+# print "if I add %d,%d,and %d I get %d." % (my_age,my_height,my_weight,my_age + my_height + my_weight)
+
+name = 'Zed A. Shaw'
+age = 35 #not a lie
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'white'
+hair = 'Brown'
+
+print "let's talk about %s." % name
+print "he's %d inches tall." % height
+print "he's %d pounds heavy." % weight
+print "actully that's not too heavy"
+print "he's got %s eyes and %s hair." % (eyes,hair)
+print "his teeth are usually %s depending on the coffee." % teeth
+
+print "if I add %d,%d,and %d I get %d" % (age,height,weight,age+height+weight)
