ff226b79bcdf1d860bba8b9349270bd63a58f512
alina <alina@admins-iMac.local>
2018-06-08 10:22:04 +0800

    第一题

--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
+#--coding:utf-8--
+
 print "hello world!"
 print "hello again"
 print "ilike typing this"
 print "this is fun"
 print "yay!printing"
-print "i'd much rather you 'not'"
-
+print "i'd much rather you 'not'."
+print 'i"said" do not touch this'
