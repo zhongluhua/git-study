96f5b32549eaa145111ee095ba43416078b57444
alina <zhongluhua@talicai.com>
2018-06-11 09:55:27 +0800

    add ex3.py

---
+++ ex3.py
@@ -0,0 +1,40 @@
+#--coding:utf-8--
+# +plus 加号
+# -minus 减号
+# /slash 斜杠
+# *asterisk 星号
+# %percent 百分号
+# <less-than 小于号
+# >greater-than 大于号
+# <=less-than-equal 小于等于号
+# >=greater-than-equal 大于等于号
+
+print "i will now count my chickens:"
+print "hens", 25 + 30 /6
+print "roosters", 100 - 25 * 3 % 4
+print "now i will count the eggs:"
+print 3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6
+print "is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 -7
+print "oh,that's why it's false."
+print "how about some more."
+print "is it greater?", 5 > -2
+print "is it greater or equal?", 5 >= -2
+print "is it less or equal?", 5 <= -2
+
+print "i will now count my chickens:"
+print "hens", 25.0 +30.0 / 6.0
+print "roosters", 100.0 -25.0 * 3.0 % 4.0
+print "now i will count the eggs:"
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+print "is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+print 3.0 + 2.0 < 5.0 - 7.0
+print "what is 3.0 + 2.0?", 3.0 + 2.0
+print "what is 5.0 - 7.0?", 5.0 - 7.0
+print "oh,that's why it's false."
+print "how about some more."
+print "is it greater?",5.0 > -2.0
+print "is it greater or equal?", 5.0 >= -2.0
+print "is it less or equal?", 5.0 <= -2.0
