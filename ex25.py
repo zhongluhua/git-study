4585709e9025e9b2d09e9746bdae859988d063c6
alina <alina@alinas-iMac.local>
2021-03-30 10:06:45 +0800

    修改25、26

--- ex25.py
+++ ex25.py
@@ -1,22 +1,38 @@
+#coding=utf-8
 def break_words(stuff):
     """This function will break up words for us."""
-    words = stuff.split(' ')
+    words = stuff.split(' ') #split()通过指定分隔符对字符串进行切片，此处是通过空格切片
     return words
 
+"""sort和sorted区别：sort是应用在list上的方法，sorted可以对所有可迭代的对象进行排序操作
+list的sort方法返回的是对已经存在的列表进行操作，而内建函数sorted方法返回的是一个新的list，而不是在原来的基础上进行操作
+sorted(iterable,key=None,reverse=False)
+iterable:可迭代对象
+key:主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序
+reverse：排序规则，reverse =True 降序，reverse = False 升序(默认)
+#返回值：返回重新排序的列表
+"""
 def sort_words(words):
     """Sorts the words."""
-    return sorted(words)
+    return sorted(words) #sorted()函数对所有可迭代对的对象进行排序操作
+
+
+"""pop()函数用于移除列表中的一个元素(默认最后一个元素)，并且返回该元素的值
+list.pop([index=-1])，obj可选参数，要移除列表元素的索引值，不能超过列表总长度，默认为index=-1,删除最后一个列表值
+"""
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.pop(0)
+    word = words.pop(0) #移除第一个元素
     print word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1)
+    word = words.pop(-1) #移除最后一个元素
     print word
 
+
+#结合break_words和sort_words
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sortedwords."""
     words = break_words(sentence)
--- ex26.py
+++ ex26.py
@@ -1,92 +1,100 @@
-def break_words(stuff):
-    """This function will break up words for us."""
-    words = stuff.split(' ')
-    return words
- 
-def sort_words(words):
-    """Sorts the words."""
-    return sorted(words)
- 
-def print_first_word(words):
-    """Prints the first word after popping it off."""
-    word = words.pop(0)
-    print(word)
- 
-def print_last_word(words):
-    """Prints the last word after popping it off."""
-    word = words.pop(-1)
-    print(word)
- 
-def sort_sentence(sentence):
-    """Takes in a full sentence and returns the sorted words."""
-    words = break_words(sentence)
-    return sort_words(words)
- 
-def print_first_and_last(sentence):
-    """Prints the first and last words of the sentence."""
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
- 
-def print_first_and_last_sorted(sentence):
-    """Sorts the words then prints the first and last one."""
-    words = sort_sentence(sentence)
-    print_first_word(words)
-    print_last_word(words)
- 
- 
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
- 
+from sys import argv
+
+print("How old are you?")
+age = input()
+print("How tall are you?")
+tall = raw_input()
+print("How much do you weigh?")
+weight = raw_input()
+
+print("So, you're %s old, %s tall and %s heavy." % (age,tall,weight))
+
+script, filename = argv
+
+txt = open(filename)
+
+print("Here's your file %s:" % filename)
+print(txt.read())
+
+print("Type the filename again:")
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+
+
+print('Let\'s practice everything.')
+print("""You'd need to know \'bout escapes 
+      with \ that do \n newlines and \t tabs.""")
+
 poem = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
-and requires an explantion
+and requires an explanation
 \n\t\twhere there is none.
 """
- 
- 
+
 print("--------------")
 print(poem)
 print("--------------")
- 
-five = 10 - 2 + 3 - 5
-print("This should be five: %s" % five)
- 
+
+
+five = 10 - 2 + 3 - 1
+print("This should be five: %d" % five)
+
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
- 
- 
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
- 
-print("With a starting point of: %d" % start_point)
-print("We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates))
- 
-start_point = start_point / 10
- 
-print("We can also do that this way:")
-print("We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point))
- 
- 
-sentence = "All good \tthings come to those who wait."
- 
- 
-words = break_words(sentence)
-sorted_words = sort_words(words)
- 
-print_first_word(words)
-print_last_word(words)
-print_first_word(sorted_words)
-print_last_word(sorted_words)
-sorted_words = sort_sentence(sentence)
-print(sorted_words)
- 
-print_first_and_last(sentence)
-print_first_and_last_sorted(sentence)
 
+
+start_point = 10000
+beans, jars,crates = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print("We'd have %s beans, %s jars, and %s crates." % (beans,jars,crates))
+
+start_point = start_point / 10
+
+print("We can also do that this way:")
+formula = secret_formula(start_point)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans,{} jars, and {} crates.".format(*formula))
+
+
+
+people = 20
+cates = 30
+dogs = 15
+
+
+if people < cates:
+    print "Too many cats! The world is doomed!"
+
+if people < cates:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs.")
